b5043d4c44c870ac79179f8bdc2e615a291e7ac1
Artem <105312810+Spellk@users.noreply.github.com>
2023-05-10 17:37:04 +0300

    fix: update about block error

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,6 +1,5 @@
 const fs = require("fs").promises;
 const path = require("path");
-
 const componentsDir = path.join(__dirname, "components");
 const stylesDir = path.join(__dirname, "styles");
 const assetsDir = path.join(__dirname, "assets");
@@ -8,30 +7,28 @@ const tempFile = path.join(__dirname, "template.html");
 const indexFile = "index.html";
 const styleFile = "style.css";
 const distDir = path.join(__dirname, "project-dist");
-
 async function replaceTemplateTags(template, components) {
   const componentRegex = /{{\s*(\w+)\s*}}/g;
   let result = template;
   let match;
-
   while ((match = componentRegex.exec(template)) !== null) {
     const [fullMatch, componentName] = match;
     const componentPath = path.join(componentsDir, `${componentName}.html`);
     const componentContent = await fs.readFile(componentPath, "utf-8");
     result = result.replace(fullMatch, componentContent);
   }
-
   return result;
 }
 
 async function buildPage() {
   try {
+    await fs.mkdir(distDir);
+
     const templateContent = await fs.readFile(tempFile, "utf-8");
     const components = await fs.readdir(componentsDir);
     const indexContent = await replaceTemplateTags(templateContent, components);
     const indexPath = path.join(distDir, indexFile);
     await fs.writeFile(indexPath, indexContent);
-
     const styleFiles = await fs.readdir(stylesDir);
     const styleContent = await Promise.all(
       styleFiles.map((file) => fs.readFile(path.join(stylesDir, file), "utf-8"))
@@ -41,11 +38,7 @@ async function buildPage() {
 
     const assetFiles = await fs.readdir(assetsDir);
     const assetDir = path.join(distDir, "assets");
-    try {
-      await fs.access(assetDir);
-    } catch {
-      await fs.mkdir(assetDir);
-    }
+    await fs.mkdir(assetDir);
     for (let file of assetFiles) {
       const sourcePath = path.join(assetsDir, file);
       const destPath = path.join(assetDir, file);
@@ -56,7 +49,6 @@ async function buildPage() {
         await fs.copyFile(sourcePath, destPath);
       }
     }
-
     console.log("Page build successfully");
   } catch (error) {
     console.error("Error building page:", error);
@@ -64,15 +56,7 @@ async function buildPage() {
 }
 
 async function copyFolder(src, dest) {
-  try {
-    await fs.mkdir(dest);
-  } catch (error) {
-    if (error.code === "EEXIST") {
-      return;
-    }
-    throw error;
-  }
-
+  await fs.mkdir(dest);
   const entries = await fs.readdir(src, { withFileTypes: true });
   for (let entry of entries) {
     const sourcePath = path.join(src, entry.name);
@@ -84,5 +68,4 @@ async function copyFolder(src, dest) {
     }
   }
 }
-
 buildPage();
